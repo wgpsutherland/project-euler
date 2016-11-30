bd9e0c556b898745afef3141953de9e405078a02
Will Sutherland <wgpsutherland@gmail.com>
2016-11-30 23:10:46 +0000

    Make question 10 a little nicer

--- 10.py
+++ 10.py
@@ -9,11 +9,9 @@ def is_prime(x):
 
     if x < 2:
         return False
-    
-    if x == 2:
+    elif x == 2:
         return True
-
-    if x % 2 == 0:
+    elif x % 2 == 0:
         return False
 
     for i in xrange(3, int(x ** 0.5) + 1, 2):
